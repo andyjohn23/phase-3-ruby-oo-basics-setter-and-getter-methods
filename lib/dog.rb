1c4cd8f498ddb5718fbc06a27394fb0703a755c4
andyjohn23 <andrewnick906@gmail.com>
2022-06-02 09:46:29 +0300

    completed the tests successfully

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
 class Dog
+    def name=(name)
+        @name = name
+    end
 
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
 end
+
+r1 = Dog.new
+r1.name = "Bosco"
+
+r2 = Dog.new
+r2.breed = "Chiwawa"
+
+puts r1.name
+puts r2.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,26 @@
 class Person
+    def name=(name)
+        @name = name
+    end
 
+    def job=(job)
+        @job = job
+    end
+
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
 end
+
+p1 = Person.new
+p1.name = "Andy"
+
+p2 = Person.new
+p2.job = "Web Developer"
+
+puts p1.name
+puts p2.job
